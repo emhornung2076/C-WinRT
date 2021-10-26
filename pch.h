88a2b55933e61f26bed1fa8ad4f03bfab37e9682
emhornung2076 <89880653+emhornung2076@users.noreply.github.com>
2021-10-26 16:48:00 -0400

    Add files via upload

---
+++ pch.h
@@ -0,0 +1,9 @@
+﻿// Erik Hornung
+// https://docs.microsoft.com/en-us/windows/uwp/cpp-and-winrt-apis/get-started?view=msvc-160
+// 
+// pch.h
+#pragma once
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Web.Syndication.h>
+#include <iostream>
+#include <winrt/base.h>
